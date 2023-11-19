6e44689dd4d337a92f1c1de722354e4211a1a45f
daan0220 <daikisoccer02@icloud.com>
2023-11-19 19:15:29 +0900

    Implementation of cart functionality

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :name
     validates :status
@@ -12,4 +12,5 @@ enum status: {
   withdrawn: 1,
   banned: 2
   }
+  has_many :cart_items, dependent: :destroy
 end
